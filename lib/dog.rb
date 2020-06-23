62ff6439aa90b4133b7c4ab93f6f1fafad69e98a
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-23 20:45:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,7 +20,12 @@ class Dog
     @breed 
   end   
   
-  @breed ==  "Mutt"
-
+  if @breed ==  
+    "Mutt"
+  end   
 end 
+
+#Additionally, `Dog#initialize` should accept a second optional argument for the
+#dog's breed stored in an instance variable `@breed`. When none is provided, it
+#should default to "Mutt".
 
